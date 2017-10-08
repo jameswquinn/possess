d4da08a4d68be2b3014896f1dacf2aee361c3e1b
jameswquinn <jameswquinn@users.noreply.github.com>
2017-10-08 14:50:11 +0100

    update

--- src/app/index.js
+++ src/app/index.js
@@ -2,10 +2,3 @@
  * Created by matt on 2/19/17.
  */
 import $ from 'jquery';
-const $main = $('#main');
-
-export const render = (markup) => $main.append(markup());
-
-export const pageInit = (markup) => {
-  $(document).ready(() => render(markup));
-};
--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const webpack = require('webpack');
 const ETP = require('extract-text-webpack-plugin');
 const PurifyCSSPlugin = require('purifycss-webpack');
+const FaviconsWebpackPlugin = require('favicons-webpack-plugin')
 
 module.exports = {
     context: path.join(__dirname, 'src'),
@@ -113,6 +114,39 @@ module.exports = {
         new webpack.optimize.CommonsChunkPlugin({
             name: 'common'
         }),
+        new FaviconsWebpackPlugin({
+    // Your source logo
+    logo: './logo.png',
+    // The prefix for all image files (might be a folder or a name)
+    prefix: 'icons-[hash]/',
+    // Emit all stats of the generated icons
+    emitStats: false,
+    // The name of the json containing all favicon information
+    statsFilename: 'iconstats-[hash].json',
+    // Generate a cache file with control hashes and
+    // don't rebuild the favicons until those hashes change
+    persistentCache: true,
+    // Inject the html into the html-webpack-plugin
+    inject: true,
+    // favicon background color (see https://github.com/haydenbleasel/favicons#usage)
+    background: '#fff',
+    // favicon app title (see https://github.com/haydenbleasel/favicons#usage)
+    title: 'Webpack App',
+
+    // which icons should be generated (see https://github.com/haydenbleasel/favicons#usage)
+    icons: {
+      android: true,
+      appleIcon: true,
+      appleStartup: false,
+      coast: false,
+      favicons: true,
+      firefox: false,
+      opengraph: false,
+      twitter: false,
+      yandex: false,
+      windows: false
+    }
+  }),
 
         // new ETP('styles.css'),
         new ETP({
@@ -125,6 +159,6 @@ module.exports = {
             purifyOptions: {
                 minify: true
             }
-        })
+        }),
     ]
 };
